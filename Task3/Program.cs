5be50b84e57ca7cdc330abd74a6bf530827aace7
Ivan Dzevetckiy <dzevetckiy@gmail.com>
2024-03-21 14:06:13 +0300

    lp3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintReverse(int[] arr, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+        Console.WriteLine(arr[index]);
+        PrintReverse(arr, index - 1);
+    }
+
+    {
+        int[] arr = new int[] {5, 4, 3, 2, 1, -1, -2, -3, -4, -5};
+        PrintReverse(arr, arr.Length - 1);
+    }
