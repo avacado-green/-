13ab75c5d3b1f77297b3dc9b1ba0e3e4df8bbe6b
Ivan Dzevetckiy <dzevetckiy@gmail.com>
2024-03-21 14:03:50 +0300

    lp2

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintNumbers(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.WriteLine(M);
+            PrintNumbers(M + 1, N);
+        }
+    }
+
+    {
+        int M = 2;  
+        int N = 20; 
+
+        PrintNumbers(M, N);
+    }
